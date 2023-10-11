a7e2053a9b54633e9f7950506659aa04549f9c1f
Task-TeamB <Task-TeamB@example.com>
2023-10-11 14:17:17 +0000

    googleAPI 祝日カレンダー連携

--- calendar_app/static/calendar_js/app.js
+++ calendar_app/static/calendar_js/app.js
@@ -3,25 +3,41 @@ document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
+        
         initialView: 'dayGridMonth',
         locale: 'ja',
         headerToolbar: {
           left: 'prev,next today',
           center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+          right: 'dayGridMonth,timeGridWeek,timeGridDay,listDay'
         },
         buttonText: {
             today: '今月',
             month: '月',
             week: '週',
             day: '日',
-            list: 'リスト'
-          },
+            list: 'リスト',
+            
+        },
         navLinks: true, // can click day/week names to navigate views
         businessHours: true, // display business hours
         editable: true,
         selectable: true,
+        
+        // googleApi連携
+        googleCalendarApiKey: "AIzaSyAkL4UivND5nu3usAu3faTP1O-8YHxH9Uo",
+	    eventSources: [
+    		//自分のカレンダー
+    		{
+    		    //googleCalendarId: 'naoto.light@gmail.com'
+    		},
+    		//祝日カレンダー
+    		{
+    			googleCalendarId: 'ja.japanese#holiday@group.v.calendar.google.com'
+    		}
+	    ]
+	    
     });
-
+    
     calendar.render();
 });
