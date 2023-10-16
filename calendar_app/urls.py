23728223aae1f32f22533b4ccd73ba06dfe9eee7
Task-TeamB <Task-TeamB@example.com>
2023-10-16 12:57:55 +0000

    Bootstrap導入

--- calendar_app/urls.py
+++ calendar_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+
 app_name = "cal"
 urlpatterns = [
-    path("sc/", views.index, name="index"),
+    path("sc/", views.calendar, name="calendar"),
 ]
--- calendar_app/views.py
+++ calendar_app/views.py
@@ -2,10 +2,16 @@
 from django.template import loader
 from django.http import HttpResponse
 
+#ログイン機能
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
-def index(request):
+@login_required #ログイン機能
+
+def calendar(request):
     """
     カレンダー画面
     """
-    template = loader.get_template("calendar_app/index.html")
+    template = loader.get_template("calendar_app/calendar.html")
     return HttpResponse(template.render())
